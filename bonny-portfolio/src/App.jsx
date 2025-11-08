cce32095495459e86608f5153a38c975b33e042e
bonny2long <bmakaniankhondo@ivytech.edu>
2025-11-08 15:14:43 -0600

    Initial scaffold: React + Vite + Tailwind setup

---
+++ bonny-portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-linear-to-br from-slate-900 to-gray-800 text-white">
+      <h1 className="text-4xl font-bold">Tailwind v4 is working </h1>
+    </div>
+  );
+}
